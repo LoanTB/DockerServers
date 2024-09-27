517888b53ce11bd2b8b07607548def7f852cfdb2
katia_auxilien <katia.auxilien@etu.umontpellier.fr>
2024-09-27 15:17:40 +0200

    Remove first arg : TOKEN

--- scripts/init-node.sh
+++ scripts/init-node.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+if [ "$#" -ne 2 ]; then
+  echo "Erreur : Ce script nécessite exactement 2 arguments."
+  echo "Usage : $0 <ip> <port>"
+  exit 1
+fi
 
-echo "Enter token : $TOKEN"
-echo "Enter host ip : $HOST"
-echo "Enter port : $PORT"
+IP_PORT=$2
+AUTRE_ARGUMENT=$3
+
+#add un scp pour récup le $token
 
 docker swarm join --token $TOKEN $HOST:$PORT

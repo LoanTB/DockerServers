304cf30074570948c3adc88ea1c6fc3200efa902
tremouletl <loan.tremoulet-breton01@etu.umontpellier.fr>
2024-09-27 15:41:31 +0200

    feat: Manager init script with ssh token share

---
+++ scripts/manager/init-manager.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Appeler la machine Manger sur le réseau
+hostnamectl set-hostname manager
+
+
+# Initialiser le swarm (Le lien à partager aux nodes s'affichera)
+docker swarm init --advertise-addr 10.99.0.1 | tail -4 | head -1 > /tmp/worker-join-command.txt
---
+++ scripts/manager/scan-manager.sh
@@ -0,0 +1,1 @@
+watch docker node ls
